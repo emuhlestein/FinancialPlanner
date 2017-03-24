c23a76d6b877bdda5f28c2761434bd835c4a49cc
Ed Muhlestein <emuhlestein@yahoo.com>
2017-03-24 17:41:43 -0500

    Add views to new user activity. Launch new user activity.

--- retirementhelper/src/main/java/com/intelliviz/retirementhelper/ui/MainActivity.java
+++ retirementhelper/src/main/java/com/intelliviz/retirementhelper/ui/MainActivity.java
@@ -1,15 +1,30 @@
 package com.intelliviz.retirementhelper.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.intelliviz.retirementhelper.R;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
+public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.new_user_button) Button mNewUserButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mNewUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NewUserActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
     }
 }
