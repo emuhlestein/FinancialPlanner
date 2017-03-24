7a74d5d7af8d99677937e092326ed8fb3ae946ab
Ed Muhlestein <emuhlestein@yahoo.com>
2017-03-24 18:53:41 -0500

    Name in new user layout to use TextInputLayout.

--- retirementhelper/src/main/java/com/intelliviz/retirementhelper/ui/NewUserActivity.java
+++ retirementhelper/src/main/java/com/intelliviz/retirementhelper/ui/NewUserActivity.java
@@ -2,14 +2,21 @@ package com.intelliviz.retirementhelper.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 import com.intelliviz.retirementhelper.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class NewUserActivity extends AppCompatActivity {
+    @Bind(R.id.name_edit_view) EditText mUserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
+        ButterKnife.bind(this);
+        mUserName.setHint("Name");
     }
 }
