626bcd6327a18402981a205dec0bbb52b32972e4
Ed Muhlestein <emuhlestein@yahoo.com>
2017-03-24 17:19:28 -0500

    Add new user activity. Add buttreknife.

--- retirementhelper/src/main/java/com/intelliviz/retirementhelper/ui/MainActivity.java
+++ retirementhelper/src/main/java/com/intelliviz/retirementhelper/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.intelliviz.retirementhelper;
+package com.intelliviz.retirementhelper.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.intelliviz.retirementhelper.R;
 
 public class MainActivity extends AppCompatActivity {
 
