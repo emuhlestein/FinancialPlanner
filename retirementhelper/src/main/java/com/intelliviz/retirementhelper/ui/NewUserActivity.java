dbf808eb1ff11a452bbf6e84fc3bab88699b5e0c
Ed Muhlestein <emuhlestein@yahoo.com>
2017-03-25 08:40:45 -0500

    Add subtitle for new user activity. Create empty validate methods.

--- retirementhelper/src/main/java/com/intelliviz/retirementhelper/ui/NewUserActivity.java
+++ retirementhelper/src/main/java/com/intelliviz/retirementhelper/ui/NewUserActivity.java
@@ -1,6 +1,7 @@
 package com.intelliviz.retirementhelper.ui;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -15,8 +16,21 @@ public class NewUserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
         ButterKnife.bind(this);
+
+        ActionBar ab = getSupportActionBar();
+        ab.setSubtitle("New User");
     }
 
     public void registerUser(View view) {
+        validateEmail();
+        validatePassword();
+    }
+
+    private void validatePassword() {
+
+    }
+
+    private void validateEmail() {
+
     }
 }
