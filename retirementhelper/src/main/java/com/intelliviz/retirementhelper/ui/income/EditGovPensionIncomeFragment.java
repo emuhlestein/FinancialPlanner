d37157d20400a09ec3e63c0d244f0dc2b8ecda77
Ed Muhlestein <emuhlestein@yahoo.com>
2017-05-10 19:12:19 -0500

    Rename pension fragments.

--- retirementhelper/src/main/java/com/intelliviz/retirementhelper/ui/income/EditGovPensionIncomeFragment.java
+++ retirementhelper/src/main/java/com/intelliviz/retirementhelper/ui/income/EditGovPensionIncomeFragment.java
@@ -12,10 +12,10 @@ import com.intelliviz.retirementhelper.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PensionIncomeFragment extends Fragment {
+public class EditGovPensionIncomeFragment extends Fragment {
 
 
-    public PensionIncomeFragment() {
+    public EditGovPensionIncomeFragment() {
         // Required empty public constructor
     }
 
@@ -24,7 +24,7 @@ public class PensionIncomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pension_income, container, false);
+        return inflater.inflate(R.layout.fragment_gov_pension_income, container, false);
     }
 
 }
--- retirementhelper/src/main/java/com/intelliviz/retirementhelper/ui/income/EditPensionIncomeFragment.java
+++ retirementhelper/src/main/java/com/intelliviz/retirementhelper/ui/income/EditPensionIncomeFragment.java
@@ -12,10 +12,10 @@ import com.intelliviz.retirementhelper.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class IncomeSourceFragment extends Fragment {
+public class EditPensionIncomeFragment extends Fragment {
 
 
-    public IncomeSourceFragment() {
+    public EditPensionIncomeFragment() {
         // Required empty public constructor
     }
 
@@ -24,7 +24,7 @@ public class IncomeSourceFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_income_source, container, false);
+        return inflater.inflate(R.layout.fragment_pension_income, container, false);
     }
 
 }
