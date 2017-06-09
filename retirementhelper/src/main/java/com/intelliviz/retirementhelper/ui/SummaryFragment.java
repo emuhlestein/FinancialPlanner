44a7ca5ec4c0dd99dbbd78a7ab7fe83a2e379c75
Ed Muhlestein <emuhlestein@yahoo.com>
2017-06-09 16:07:42 -0500

    Rename summary fragment layout.

--- retirementhelper/src/main/java/com/intelliviz/retirementhelper/ui/SummaryFragment.java
+++ retirementhelper/src/main/java/com/intelliviz/retirementhelper/ui/SummaryFragment.java
@@ -35,7 +35,7 @@ public class SummaryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.summary_contents, container, false);
+        View view = inflater.inflate(R.layout.fragment_summary, container, false);
         ButterKnife.bind(this, view);
 
         AppCompatActivity activity = (AppCompatActivity)getActivity();
