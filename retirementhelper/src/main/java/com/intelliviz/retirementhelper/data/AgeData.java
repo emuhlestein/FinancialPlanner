18047e1092ca7b64f6d034c533346a7003dfe65c
Ed Muhlestein <emuhlestein@yahoo.com>
2017-07-01 10:53:08 -0500

    Clean up agedata class.

--- retirementhelper/src/main/java/com/intelliviz/retirementhelper/data/AgeData.java
+++ retirementhelper/src/main/java/com/intelliviz/retirementhelper/data/AgeData.java
@@ -4,42 +4,47 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 /**
- * Created by edm on 5/23/2017.
+ * Class to handle ages.
+ * Created by Ed Muhlestein on 5/23/2017.
  */
 
 public class AgeData implements Parcelable {
     private int mYear;
     private int mMonth;
 
+    /**
+     * Default constructor.
+     */
     public AgeData() {
         mYear = 0;
         mMonth = 0;
     }
 
+    /**
+     * Constructor.
+     *
+     * @param year The year.
+     * @param month The month.
+     */
     public AgeData(int year, int month) {
         mYear = year;
         mMonth = month;
     }
 
     /**
-     * Does ageDate come on or before this date.
-     * @param age
-     * @return
+     * Does this age come on or before the specified age.
+     * @param age The age to check.
+     * @return True if age comes before this age. False otherwise.
      */
     public boolean isBefore(AgeData age) {
-        if(mYear < age.getYear()) {
-            return true;
-        } else if(mYear == age.getYear()) {
-            if(mMonth < age.getMonth() ) {
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
+        return (mYear < age.getYear() ||  (mYear == age.getYear() && mMonth < age.getMonth()));
     }
 
+    /**
+     * Subtract the specified age from this age.
+     * @param ageData The specified age.
+     * @return The age difference.
+     */
     public AgeData subtract(AgeData ageData) {
         int year = mYear - ageData.getYear();
         int month = mMonth - ageData.getMonth();
@@ -51,30 +56,39 @@ public class AgeData implements Parcelable {
         return new AgeData(year, month);
     }
 
+    /**
+     * Get the number of months.
+     * @return THe number of months.
+     */
     public int getNumberOfMonths() {
         return mYear * 12 + mMonth;
     }
 
+    /**
+     * Get the year.
+     * @return The year.
+     */
     public int getYear() {
         return mYear;
     }
 
+    /**
+     * Get the month.
+     * @return The month.
+     */
     public int getMonth() {
         return mMonth;
     }
 
-    public boolean isValid() {
-        if(mYear > 0 && mMonth > 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
+    /**
+     * Constuctor used by parcelable.
+     * @param in THe input parcel from which to extract the values.
+     */
     public AgeData(Parcel in) {
         readFromParcel(in);
     }
 
+    @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append(Integer.toString(mYear));
@@ -84,6 +98,10 @@ public class AgeData implements Parcelable {
         return sb.toString();
     }
 
+    /**
+     * Get the unformated string.
+     * @return The unformatted string.
+     */
     public String getUnformattedString() {
         StringBuilder sb = new StringBuilder();
         sb.append(Integer.toString(mYear));
@@ -92,12 +110,10 @@ public class AgeData implements Parcelable {
         return sb.toString();
     }
 
-
     @Override
     public int describeContents() {
         return 0;
     }
-
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
@@ -105,6 +121,10 @@ public class AgeData implements Parcelable {
         dest.writeInt(mMonth);
     }
 
+    /**
+     * Read the values from the parcel.
+     * @param in THe parcel.
+     */
     public void readFromParcel(Parcel in) {
         mYear = in.readInt();
         mMonth = in.readInt();
