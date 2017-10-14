17354f33c1a0dadb2fe49560adb3e4705f37f72c
Ed Muhlestein <emuhlestein@yahoo.com>
2017-10-14 12:26:13 -0500

    gov pension income to use activity and not fragment.

--- retirementhelper/src/main/java/com/intelliviz/retirementhelper/ui/income/GovPensionIncomeActivity.java
+++ retirementhelper/src/main/java/com/intelliviz/retirementhelper/ui/income/GovPensionIncomeActivity.java
@@ -1,28 +1,62 @@
 package com.intelliviz.retirementhelper.ui.income;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.intelliviz.retirementhelper.R;
-import com.intelliviz.retirementhelper.util.RetirementConstants;
+import com.intelliviz.retirementhelper.data.AgeData;
+import com.intelliviz.retirementhelper.db.entity.GovPensionEntity;
+import com.intelliviz.retirementhelper.util.SystemUtils;
+import com.intelliviz.retirementhelper.viewmodel.GovPensionViewModel;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static com.intelliviz.retirementhelper.util.RetirementConstants.EXTRA_INCOME_SOURCE_ID;
+import static com.intelliviz.retirementhelper.util.RetirementConstants.INCOME_TYPE_GOV_PENSION;
+import static com.intelliviz.retirementhelper.util.SystemUtils.getFloatValue;
 
 public class GovPensionIncomeActivity extends AppCompatActivity {
+
+    private GovPensionEntity mGPID;
+    private long mId;
+    private GovPensionViewModel mViewModel;
+
+    @Bind(R.id.coordinatorLayout)
+    CoordinatorLayout mCoordinatorLayout;
+
+    @Bind(R.id.name_edit_text)
+    EditText mIncomeSourceName;
+
+    @Bind(R.id.min_age_text)
+    EditText mMinAge;
+
+    @Bind(R.id.monthly_benefit_text)
+    EditText mMonthlyBenefit;
 
     @Bind(R.id.income_source_toolbar)
     Toolbar mToolbar;
 
+    @OnClick(R.id.add_income_source_button) void onAddIncomeSource() {
+        updateIncomeSourceData();
+        finish();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gov_pension_income);
+        setContentView(R.layout.fragment_edit_gov_pension_income);
 
 
         ButterKnife.bind(this);
@@ -30,33 +64,77 @@ public class GovPensionIncomeActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
 
         Intent intent = getIntent();
-        int mIncomeSourceType = intent.getIntExtra(RetirementConstants.EXTRA_INCOME_SOURCE_TYPE, RetirementConstants.INCOME_TYPE_SAVINGS);
-        int mIncomeSourceAction = intent.getIntExtra(RetirementConstants.EXTRA_INCOME_SOURCE_ACTION, RetirementConstants.INCOME_ACTION_VIEW);
-
-        if(mIncomeSourceAction == RetirementConstants.INCOME_ACTION_ADD) {
-            addIncomeSourceFragment(intent);
-        } else {
-            // View or edit an income source
-            if(mIncomeSourceAction == RetirementConstants.INCOME_ACTION_EDIT) {
-                // View or edit an income source
-                addIncomeSourceFragment(intent);
-            } else {
-                addIncomeSourceFragment(intent);
-            }
+        mId = 0;
+        if(intent != null) {
+            mId = intent.getLongExtra(EXTRA_INCOME_SOURCE_ID, 0);
         }
+
+        mMonthlyBenefit.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus) {
+                    TextView textView = (TextView)v;
+                    String str = textView.getText().toString();
+                    String value = getFloatValue(str);
+                    String formattedString = SystemUtils.getFormattedCurrency(value);
+                    if(formattedString != null) {
+                        mMonthlyBenefit.setText(formattedString);
+                    }
+                }
+            }
+        });
+
+        GovPensionViewModel.Factory factory = new
+                GovPensionViewModel.Factory(getApplication(), mId);
+        mViewModel = ViewModelProviders.of(this, factory).
+                get(GovPensionViewModel.class);
+
+        mViewModel.getData().observe(this, new Observer<GovPensionEntity>() {
+            @Override
+            public void onChanged(@Nullable GovPensionEntity govPensionIncomeData) {
+                mGPID = govPensionIncomeData;
+                updateUI();
+            }
+        });
     }
 
-    private void addIncomeSourceFragment(Intent intent) {
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction ft;
-        Fragment fragment;
-
-        fragment = fm.findFragmentByTag(EditGovPensionIncomeFragment.EDIT_GOVPENSION_INCOME_FRAG_TAG);
-        if (fragment == null) {
-            fragment = EditGovPensionIncomeFragment.newInstance(intent);
-            ft = fm.beginTransaction();
-            ft.add(R.id.content_frame, fragment, EditGovPensionIncomeFragment.EDIT_GOVPENSION_INCOME_FRAG_TAG);
-            ft.commit();
+    private void updateUI() {
+        if(mGPID == null || mGPID.getId() == 0) {
+            return;
         }
+        String name = mGPID.getName();
+        String monthlyBenefit = SystemUtils.getFormattedCurrency(mGPID.getFullMonthlyBenefit());
+        String age = mGPID.getMinAge();
+
+        mIncomeSourceName.setText(name);
+        mMinAge.setText(age);
+        mMonthlyBenefit.setText(monthlyBenefit);
+
+        int type = mGPID.getType();
+        String incomeSourceTypeString = SystemUtils.getIncomeSourceTypeString(this, type);
+        SystemUtils.setToolbarSubtitle(this, incomeSourceTypeString);
+    }
+
+    private void updateIncomeSourceData() {
+        String name = mIncomeSourceName.getText().toString();
+        String minimumAge = mMinAge.getText().toString();
+        String value = mMonthlyBenefit.getText().toString();
+        String benefit = getFloatValue(value);
+        if(benefit == null) {
+            Snackbar snackbar = Snackbar.make(mCoordinatorLayout, getString(R.string.monthly_benefit_not_valid) + value, Snackbar.LENGTH_LONG);
+            snackbar.show();
+            return;
+        }
+
+        minimumAge = SystemUtils.trimAge(minimumAge);
+        AgeData minAge = SystemUtils.parseAgeString(minimumAge);
+        if(minAge == null) {
+            Snackbar snackbar = Snackbar.make(mCoordinatorLayout, getString(R.string.age_not_valid) + " " + value, Snackbar.LENGTH_LONG);
+            snackbar.show();
+            return;
+        }
+
+        GovPensionEntity gpid = new GovPensionEntity(mId, INCOME_TYPE_GOV_PENSION, name, minimumAge, benefit);
+        mViewModel.setData(gpid);
     }
 }
