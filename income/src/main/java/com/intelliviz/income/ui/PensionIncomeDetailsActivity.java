4580092ec40469a71d971c86848930de019fe6e8
Ed Muhlestein <emuhlestein@yahoo.com>
2018-10-06 14:41:27 -0500

    Cleanup code.

--- income/src/main/java/com/intelliviz/income/ui/PensionIncomeDetailsActivity.java
+++ income/src/main/java/com/intelliviz/income/ui/PensionIncomeDetailsActivity.java
@@ -14,12 +14,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.intelliviz.data.IncomeDetails;
 import com.intelliviz.data.PensionData;
 import com.intelliviz.income.R;
 import com.intelliviz.income.data.PensionViewData;
@@ -28,9 +26,6 @@ import com.intelliviz.lowlevel.data.AgeData;
 import com.intelliviz.lowlevel.util.RetirementConstants;
 import com.intelliviz.lowlevel.util.SystemUtils;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static com.intelliviz.lowlevel.util.RetirementConstants.EXTRA_INCOME_SOURCE_ID;
 import static com.intelliviz.lowlevel.util.RetirementConstants.INCOME_TYPE_PENSION;
 import static com.intelliviz.lowlevel.util.RetirementConstants.OWNER_PRIMARY;
@@ -38,21 +33,15 @@ import static com.intelliviz.lowlevel.util.RetirementConstants.OWNER_SPOUSE;
 
 
 public class PensionIncomeDetailsActivity extends AppCompatActivity {
-    private List<IncomeDetails> mIncomeDetails;
     private PensionIncomeViewModel mViewModel;
     private PensionData mPD;
     private long mId;
     private boolean mSpouseIncluded;
-
-    private Toolbar mToolbar;
-    private android.support.design.widget.AppBarLayout mAppBarLayout;
     private TextView mNameTextView;
     private TextView mStartAgeTextView;
     private TextView mMonthlyBenefitTextView;
     private LinearLayout mExpandedTextLayout;
     private CollapsingToolbarLayout mCollapsingToolbarLayout;
-    private FloatingActionButton mEditPensionFAB;
-    private RecyclerView mRecyclerView;
     private TextView mOwnerTextView;
 
     @Override
@@ -60,15 +49,14 @@ public class PensionIncomeDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pension_income_details);
 
-        mToolbar = findViewById(R.id.income_source_toolbar);
         android.support.design.widget.AppBarLayout mAppBarLayout = findViewById(R.id.appbar);
         mNameTextView = findViewById(R.id.name_text_view);
         mStartAgeTextView = findViewById(R.id.min_age_text_view);
         mMonthlyBenefitTextView = findViewById(R.id.monthly_benefit_text_view);
         mExpandedTextLayout = findViewById(R.id.expanded_text_layout);
         mCollapsingToolbarLayout = findViewById(R.id.collapsing_toolbar);
-        mEditPensionFAB = findViewById(R.id.editPensionFAB);
-        mRecyclerView = findViewById(R.id.recyclerview);
+        FloatingActionButton editPensionFAB = findViewById(R.id.editPensionFAB);
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
         mOwnerTextView = findViewById(R.id.owner_text);
 
         Intent intent = getIntent();
@@ -100,12 +88,11 @@ public class PensionIncomeDetailsActivity extends AppCompatActivity {
             }
         });
 
-        mIncomeDetails = new ArrayList<>();
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        mRecyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setLayoutManager(linearLayoutManager);
 
         // The FAB will pop up an activity to allow a new income source to be edited
-        mEditPensionFAB.setOnClickListener(new View.OnClickListener() {
+        editPensionFAB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(PensionIncomeDetailsActivity.this, PensionIncomeEditActivity.class);
@@ -123,6 +110,9 @@ public class PensionIncomeDetailsActivity extends AppCompatActivity {
         mViewModel.get().observe(this, new Observer<PensionViewData>() {
             @Override
             public void onChanged(@Nullable PensionViewData viewData) {
+                if(viewData == null) {
+                    return;
+                }
                 mSpouseIncluded = viewData.isSpouseIncluded();
                 mPD = viewData.getPensionData();
                 updateUI();
@@ -144,6 +134,9 @@ public class PensionIncomeDetailsActivity extends AppCompatActivity {
 
         if(requestCode == RetirementConstants.ACTIVITY_RESULT) {
             Bundle bundle = intent.getExtras();
+            if(bundle == null) {
+                return;
+            }
             String name = bundle.getString(RetirementConstants.EXTRA_INCOME_SOURCE_NAME);
             AgeData minAge = bundle.getParcelable(RetirementConstants.EXTRA_INCOME_SOURCE_START_AGE);
             String monthlyBenefit = bundle.getString(RetirementConstants.EXTRA_INCOME_SOURCE_BENEFIT);
@@ -167,9 +160,9 @@ public class PensionIncomeDetailsActivity extends AppCompatActivity {
         if(!mSpouseIncluded) {
             mOwnerTextView.setVisibility(View.GONE);
         } else if(mPD.getOwner() == OWNER_PRIMARY) {
-            mOwnerTextView.setText("Self");
+            mOwnerTextView.setText(getResources().getString(R.string.self));
         } else if(mPD.getOwner() == OWNER_SPOUSE) {
-            mOwnerTextView.setText("Spouse");
+            mOwnerTextView.setText(getResources().getString(R.string.spouse));
         }
 
         mNameTextView.setText(name);
